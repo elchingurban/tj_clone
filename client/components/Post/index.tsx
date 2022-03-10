ffbda7f75214aa2a9c782b6133634be1a9c923d5
elchingurban <elchinerkingurbanli@gmail.com>
2022-03-11 01:54:37 +0400

    Post component

---
+++ client/components/Post/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import { Paper, Typography } from '@material-ui/core';
+import Image from 'next/image';
+
+import styles from './Post.module.scss';
+
+export const Post: React.FC = () => {
+  return (
+    <Paper elevation={0} className="p-20" classes={{ root: styles.paper }}>
+      <Typography variant="h5" className={ styles.title }>
+        Авиаудар по Мариуполю. Главное о последствиях и о позициях Украины и
+      </Typography>
+      <Typography className="mt-15 mb-15">
+        Украинские власти сообщают о трёх погибших, среди которых — ребёнок. В
+        России это отрицают.
+      </Typography>
+      <Image
+        src="https://leonardo.osnova.io/e0e5aa68-935a-5aaf-803e-e628318f7c23/-/preview/1300/-/format/webp/"
+        height={400}
+        width={600}
+      />
+    </Paper>
+  );
+};
