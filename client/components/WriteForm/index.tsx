d2eac208bf4d957530af1d3cd13ae45121342d9c
elchingurban <elchinerkingurbanli@gmail.com>
2022-03-18 14:57:49 +0400

    feat: Editor funcitonality for new post

---
+++ client/components/WriteForm/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import { Button, Input } from '@material-ui/core';
+import { TextsmsOutlined as MessageIcon } from '@material-ui/icons';
+import styles from './WriteForm.module.scss';
+
+const Editor = dynamic(() => import('../Editor').then(m => m.Editor), { ssr: false });
+
+interface WriteFormProps {
+  title?: string;
+}
+
+export const WriteForm: React.FC<WriteFormProps> = ({title}) => {
+  return (
+    <div>
+      <Input classes={{ root: styles.titleField }} placeholder='Заголовок' defaultValue={title} />
+      <div className={ styles.editor }>
+        <Editor />
+      </div>
+      <Button variant="contained" color="primary">
+              {/* <MessageIcon className="mr-10" /> */}
+              Опубликовать
+      </Button>
+    </div>
+  )
+}
