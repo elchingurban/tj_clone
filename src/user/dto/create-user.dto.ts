ad95ead2d04956fa8fdaa0ce85c4cc52eb0bc4aa
elchingurban <elchinerkingurbanli@gmail.com>
2022-04-04 01:01:32 +0400

    Validator improved for users

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, Length } from 'class-validator';
 export class CreateUserDto {
+  @Length(3)
   fullName: string;
+
+  @IsEmail(undefined, { message: 'Wrong email' })
   email: string;
+
+  @Length(6, 32, { message: 'Password should be at least 6 characters' })
   password?: string;
 }
