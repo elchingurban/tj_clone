46cff28fd833b7b162dede628c2b4558ab6b7502
elchingurban <elchinerkingurbanli@gmail.com>
2022-04-07 17:10:58 +0400

    Auth module with JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from 'src/user/entities/user.entity';
+
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByCond({
@@ -19,5 +25,14 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: UserEntity): Promise<any> {
+    const { password, ...userData } = user;
+    const payload = { email: user.email, id: user.id };
+    return {
+      ...userData,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
